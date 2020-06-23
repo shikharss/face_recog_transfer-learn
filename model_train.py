3286dd20c7c6601dd6845579416d6d623650f92d
shikharss <64891027+shikharss@users.noreply.github.com>
2020-06-23 13:11:09 +0530

    second

--- model_train.py
+++ model_train.py
@@ -4,80 +4,146 @@
 # In[ ]:
 
 
-from keras.models import load_model
-
-classifier = load_model('face_detector.h15')
+from keras.applications import vgg16
 
 
 # In[ ]:
 
 
-import os
-import cv2
-import numpy as np
-from os import listdir
-from os.path import isfile, join
+img_rows, img_cols = 224, 224 
+
+model = vgg16.VGG16(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
 
 
 # In[ ]:
 
 
-stored_dict = { "[0]": "shikhar ", "[1]": "aman" }
-
-stored_dict_n = {"[0]": "shikhar ", "[1]": "aman" }
-
-
-# In[ ]:
-
-
-def draw_test(name, pred, im):
-    face = stored_dict[str(pred)]
-    BLACK = [0,0,0]
-    expanded_image = cv2.copyMakeBorder(im, 80, 0, 0, 100 ,cv2.BORDER_CONSTANT,value=BLACK)
-    cv2.putText(expanded_image, face, (20, 60) , cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2)
-    cv2.imshow(name, expanded_image)
+for layer in model.layers:
+    layer.trainable = False
 
 
 # In[ ]:
 
 
-def getRandomImage(path):
-    """function loads a random image from a random folder in our test path """
-    folders = list(filter(lambda x: os.path.isdir(os.path.join(path, x)), os.listdir(path)))
-    random_directory = np.random.randint(0,len(folders))
-    path_class = folders[random_directory]
-    print("Class - " + str(path_class))
-    file_path = path + path_class
-    file_names = [f for f in listdir(file_path) if isfile(join(file_path, f))]
-    random_file_index = np.random.randint(0,len(file_names))
-    image_name = file_names[random_file_index]
-    return cv2.imread(file_path+"/"+image_name)
+model.summary()
 
 
 # In[ ]:
 
 
-for i in range(0,10):
-    input_im = getRandomImage("C://Users//KIIT//Desktop//mlops//Face Detection using VGG16//Dataset//test//")
-    input_original = input_im.copy()
-    input_original = cv2.resize(input_original, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
-    
-    input_im = cv2.resize(input_im, (224, 224), interpolation = cv2.INTER_LINEAR)
-    input_im = input_im / 255.
-    input_im = input_im.reshape(1,224,224,3) 
-    
-    # Get Prediction
-    res = np.argmax(classifier.predict(input_im, 1, verbose = 0), axis=1)
-    
-    # Show image with predicted class
-    draw_test("Prediction", res, input_original) 
-    cv2.waitKey(0)
-
-cv2.destroyAllWindows()
+def layer_adder(bottom_model, num_classes):
+    top_model = bottom_model.output
+    top_model = GlobalAveragePooling2D()(top_model)
+    top_model = Dense(1024,activation='relu')(top_model)
+    top_model = Dense(512,activation='relu')(top_model)
+    top_model = Dense(num_classes,activation='softmax')(top_model)
+    return top_model
 
 
 # In[ ]:
 
 
 
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, GlobalAveragePooling2D
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
 
+# Set our class number to 3 (Young, Middle, Old)
+num_classes = 4
+
+FC_Head = layer_adder(model, num_classes)
+
+model = Model(inputs = model.input, outputs = FC_Head)
+
+print(model.summary())
+
+
+# In[ ]:
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_data_dir = "C://Users//KIIT//Desktop//mlops//Face Detection using VGG16//Dataset//train//"
+validation_data_dir = "C://Users//KIIT//Desktop//mlops//Face Detection using VGG16//Dataset//test//"
+
+# Let's use some data augmentaiton 
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=45,
+      width_shift_range=0.3,
+      height_shift_range=0.3,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+# set our batch size (typically on most mid tier systems we'll use 16-32)
+batch_size = 1
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+
+# In[ ]:
+
+
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+                     
+checkpoint = ModelCheckpoint("face_detector.h15",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss', 
+                          min_delta = 0, 
+                          patience = 3,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+# we put our call backs into a callback list
+callbacks = [earlystop, checkpoint]
+
+# We use a very small learning rate 
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = 'Adam',
+              metrics = ['accuracy'])
+
+# Enter the number of training and validation samples here
+nb_train_samples = 280
+nb_validation_samples = 120
+
+# We only train 5 EPOCHS 
+epochs = 1
+batch_size = 1
+
+history = model.fit_generator(
+    train_generator,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    )
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
